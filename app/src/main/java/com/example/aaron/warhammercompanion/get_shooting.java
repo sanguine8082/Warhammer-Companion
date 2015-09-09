c37a5ec602a0c1f05d8413be9d6d7dfc68b3d59c
Aaron Pfalzgraf <apfalzgraf8082@gmail.com>
2015-09-08 22:36:41 -0500

    Added Spinner Element
    Added a spinner element that allows the user to select an option to reroll his misses
    This spinner currently just displays a toast

--- app/src/main/java/com/example/aaron/warhammercompanion/get_shooting.java
+++ app/src/main/java/com/example/aaron/warhammercompanion/get_shooting.java
@@ -1,5 +1,6 @@
 package com.example.aaron.warhammercompanion;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -10,8 +11,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,21 +34,15 @@ public class get_shooting extends Activity {
     static int num_shots = 0;
     static int shooter_bs = 0;
     static int hits = 0;
-    List<Integer> dice_rolls = new ArrayList<Integer>();
+    static String reroll_selected;
+    //List<Integer> dice_rolls = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_shooting);
 
-        //final Random rand = new Random();
-        //final Random generator = new Random(System.currentTimeMillis());
-        //int diceRoll = rand.nextInt(6) + 1; //uniformly distributed int from 1 to 6
-
         shotsButton = (Button)findViewById(R.id.set1);
-        skillButton = (Button)findViewById(R.id.set2);
-        fireButton = (Button)findViewById(R.id.fire);
-
         shotsButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view){
                 shotsEdit = (EditText)findViewById(R.id.num_shots);
@@ -54,6 +53,8 @@ public class get_shooting extends Activity {
                 Toast.makeText(getApplicationContext(),"Set shots to "+Integer.toString(num_shots), Toast.LENGTH_SHORT).show();
             }
         });
+
+        skillButton = (Button)findViewById(R.id.set2);
         skillButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
                 skillEdit = (EditText)findViewById(R.id.shooter_skill);
@@ -64,9 +65,13 @@ public class get_shooting extends Activity {
                 Toast.makeText(getApplicationContext(),"Set BS to "+Integer.toString(shooter_bs), Toast.LENGTH_SHORT).show();
             }
         });
+
+        fireButton = (Button)findViewById(R.id.fire);
         fireButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
                 List<Integer> dice_rolls = new ArrayList<Integer>();
+                List<Integer> hit_rolls = new ArrayList<Integer>();
+                List<Integer> miss_rolls = new ArrayList<Integer>();
                 final Random generator = new Random(System.currentTimeMillis());
                 TextView finalText = (TextView)findViewById(R.id.shotshit);
                 TextView showShots = (TextView)findViewById(R.id.shotvals);
@@ -75,10 +80,15 @@ public class get_shooting extends Activity {
                 int i = 0;
                 for (i = 0; i<num_shots; i++)
                 {
-                    dice_rolls.add(generator.nextInt(6)+1);
+                    dice_rolls.add(generator.nextInt(6) + 1);
                     if(dice_rolls.get(i)>=(6-shooter_bs+1))
                     {
                         hits++;
+                        hit_rolls.add(dice_rolls.get(i));
+                    }
+                    else
+                    {
+                        miss_rolls.add(dice_rolls.get(i));
                     }
                 }
                 finalText.setText("Hit with "+Integer.toString(hits)+" shots!");
@@ -86,6 +96,25 @@ public class get_shooting extends Activity {
 
             }
         });
+
+        Spinner spinner = (Spinner) findViewById(R.id.reroll_options);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.reroll_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                reroll_selected = (String) parent.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(),reroll_selected, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
     }
 
    /* @Override
